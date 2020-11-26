d7662c176b01ab599e479b69c363748c891e2a6e
brianngeywo <brianngeywo254@gmail.com>
2020-11-26 16:51:34 +0300

    stateful management

--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,9 @@
-// single widget app
-
-// import 'package:flutter/material.dart';
-
-// void main() => runApp(MyApp());
-
-// class MyApp extends StatelessWidget {
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Hello World Demo Application',
-//       theme: ThemeData(
-//         primarySwatch: Colors.indigo,
-//       ),
-//       home: MyHomePage(title: 'Home page'),
-//     );
-//   }
-// }
-
-// class MyHomePage extends StatelessWidget {
-//   MyHomePage({Key key, this.title}) : super(key: key);
-//   final String title;
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//         decoration: BoxDecoration(
-//           color: Colors.white,
-//         ),
-//         padding: EdgeInsets.all(50),
-//         child: Center(
-//           child: Text(
-//             'First App',
-//             style: TextStyle(
-//               color: Colors.black,
-//               letterSpacing: 0.5,
-//               fontSize: 20,
-//             ),
-//             textDirection: TextDirection.ltr,
-//           ),
-//         ));
-//   }
-// }
-
-//multiple widgets app
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -55,7 +11,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Product layout demo'),
+      home: MyHomePage(title: 'Product layout demo home page'),
     );
   }
 }
@@ -63,39 +19,184 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
   final String title;
-
-  void _showDialog(BuildContext context) {
-    showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: new Text("Message"),
-            content: new Text("Hello world"),
-            actions: <Widget>[
-              new FlatButton(
-                child: new Text("funga hii kitu"),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-              ),
-            ],
-          );
-        });
-  }
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text("gestures")),
-        body: Center(
-          child: GestureDetector(
-            onTap: () {
-              _showDialog(context);
-            },
-            child: Text('Helloworld'),
-          ),
+        appBar: AppBar(title: Text("Product Listing")),
+        body: ListView(
+          shrinkWrap: true,
+          padding: const EdgeInsets.fromLTRB(2.0, 10.0, 2.0, 10.0),
+          children: <Widget>[
+            ProductBox(
+                name: "iPhone",
+                description: "iPhone is the stylist phone ever",
+                price: 1000,
+                image: "1.jpg"),
+            ProductBox(
+                name: "Pixel",
+                description: "Pixel is the most featureful phone ever",
+                price: 800,
+                image: "2.jpg"),
+            ProductBox(
+                name: "Laptop",
+                description: "Laptop is most productive development tool",
+                price: 2000,
+                image: "3.jpg"),
+            ProductBox(
+                name: "Tablet",
+                description:
+                    "Tablet is the most useful device ever for meeting",
+                price: 1500,
+                image: "4.jpg"),
+            ProductBox(
+                name: "Pendrive",
+                description: "iPhone is the stylist phone ever",
+                price: 100,
+                image: "5.jpg"),
+            ProductBox(
+                name: "Floppy Drive",
+                description: "iPhone is the stylist phone ever",
+                price: 20,
+                image: "6.jpg"),
+            ProductBox(
+                name: "iPhone",
+                description: "iPhone is the stylist phone ever",
+                price: 1000,
+                image: "7.jpg"),
+            ProductBox(
+                name: "iPhone",
+                description: "iPhone is the stylist phone ever",
+                price: 1000,
+                image: "8.jpg"),
+          ],
         ));
   }
 }
 
+class RatingBox extends StatefulWidget {
+  @override
+  _RatingBoxState createState() => _RatingBoxState();
+}
+
+class _RatingBoxState extends State<RatingBox> {
+  int _rating = 0;
+  void _setRatingAsOne() {
+    setState(() {
+      _rating = 1;
+    });
+  }
+
+  void _setRatingAsTwo() {
+    setState(() {
+      _rating = 2;
+    });
+  }
+
+  void _setRatingAsThree() {
+    setState(() {
+      _rating = 3;
+    });
+  }
+
+  Widget build(BuildContext context) {
+    double _size = 20;
+    print(_rating);
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      crossAxisAlignment: CrossAxisAlignment.end,
+      mainAxisSize: MainAxisSize.max,
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.all(0),
+          child: IconButton(
+            icon: (_rating >= 1
+                ? Icon(
+                    Icons.star,
+                    size: _size,
+                  )
+                : Icon(
+                    Icons.star_border,
+                    size: _size,
+                  )),
+            color: Colors.red[500],
+            onPressed: _setRatingAsOne,
+            iconSize: _size,
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.all(0),
+          child: IconButton(
+            icon: (_rating >= 2
+                ? Icon(
+                    Icons.star,
+                    size: _size,
+                  )
+                : Icon(
+                    Icons.star_border,
+                    size: _size,
+                  )),
+            color: Colors.red[500],
+            onPressed: _setRatingAsTwo,
+            iconSize: _size,
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.all(0),
+          child: IconButton(
+            icon: (_rating >= 3
+                ? Icon(
+                    Icons.star,
+                    size: _size,
+                  )
+                : Icon(
+                    Icons.star_border,
+                    size: _size,
+                  )),
+            color: Colors.red[500],
+            onPressed: _setRatingAsThree,
+            iconSize: _size,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class ProductBox extends StatelessWidget {
+  ProductBox({Key key, this.name, this.description, this.price, this.image})
+      : super(key: key);
+  final String name;
+  final String description;
+  final int price;
+  final String image;
+  Widget build(BuildContext context) {
+    return Container(
+        padding: EdgeInsets.all(2),
+        height: 140,
+        child: Card(
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+              Container(
+                  child: Image.asset(
+                "assets/images/" + image,
+                fit: BoxFit.cover,
+                width: 150,
+                height: double.infinity,
+              )),
+              Expanded(
+                  child: Container(
+                      padding: EdgeInsets.all(5),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          Text(this.name,
+                              style: TextStyle(fontWeight: FontWeight.bold)),
+                          Text(this.description),
+                          Text("Price: " + this.price.toString()),
+                          RatingBox(),
+                        ],
+                      )))
+            ])));
+  }
+}
